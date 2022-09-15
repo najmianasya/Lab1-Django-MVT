c5b13f1e3b4fc05d1ba9a1f352e50c7cb8f9c8cc
Najmi Anasya Calyla <najmi.anasya@ui.ac.id>
2022-09-15 13:14:05 +0700

    Add lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 
@@ -14,3 +15,23 @@ context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Najmi Anasya Calyla'
 }
+
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
